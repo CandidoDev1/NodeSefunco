19c76824a0398069a44eb8c0ba31876950ff770f
CandidoDev1 <candido.ferreira@devtest.co.ao>
2024-07-05 15:29:34 +0100

    Adicionando ESlint

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import { http } from "node:http";
+import { http } from 'node:http';
 
 // Função que será chamada para lidar com requisições
 const requestHandler = (request, response) => {
-  // Configura o cabeçalho da resposta com status 200 e Content-Type text/plain
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  // Escreve 'Olá' como corpo da resposta
-  response.end("Olá");
-}
+    // Configura o cabeçalho da resposta com status 200 e Content-Type text/plain
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    // Escreve 'Olá' como corpo da resposta
+    response.end('Olá');
+};
 
 // Cria um servidor HTTP e define a função de requestHandler para lidar com as requisições
 const server = http.createServer(requestHandler);
@@ -16,6 +16,5 @@ const PORT = 8000;
 
 // Inicia o servidor e imprime uma mensagem indicando que está rodando
 server.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-
