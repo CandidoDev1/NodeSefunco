496fe70d9a1a8a83fd0fd16542d3390dd7861aaf
CandidoDev1 <candido.ferreira@devtest.co.ao>
2024-07-05 12:34:30 +0100

    Mudança no escript Principal (main.js), adicionando um servidor nativo na porta 8000, com uma função de resposta Olá

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
-var num = 10 
-console.log(num)
+import { http } from "node:http";
+
+// Função que será chamada para lidar com requisições
+const requestHandler = (request, response) => {
+  // Configura o cabeçalho da resposta com status 200 e Content-Type text/plain
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  // Escreve 'Olá' como corpo da resposta
+  response.end('Olá');
+}
+
+// Cria um servidor HTTP e define a função de requestHandler para lidar com as requisições
+const server = http.createServer(requestHandler);
+
+// Define a porta em que o servidor irá escutar
+const PORT = 8000;
+
+// Inicia o servidor e imprime uma mensagem indicando que está rodando
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
